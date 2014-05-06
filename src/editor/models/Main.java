abc077eb2d44175294c9050f6e6af01354a4a7e2
godexxx <godexxxx@gmail.com>
2014-05-06 21:08:11 +0300

    Rename editor

--- src/editor/models/Main.java
+++ src/editor/models/Main.java
@@ -15,7 +15,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/views/main_window.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("../views/main_window.fxml"));
         primaryStage.setTitle("Harchenko");
         primaryStage.getIcons().add(new Image("/res/img/uml_icon.png"));
         primaryStage.setScene(new Scene(root));
