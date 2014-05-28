6c2e04b520bc6bede001b38939925110d56e2d6b
godexxx <godexxxx@gmail.com>
2014-05-28 12:35:51 +0300

    Модуль перебора,переписан

--- src/editor/services/gen_arch_done.java
+++ src/editor/services/gen_arch_done.java
@@ -9,14 +9,14 @@ import java.util.ArrayList;
  * Created by godex_000 on 27.05.2014.
  */
 public class gen_arch_done {
-    public static ArrayList<Architecture> pre_combine(Architecture origin_arch, ArrayList<Module> modules_arr_not_selected) {
+    public static ArrayList<Architecture> pre_combine(Architecture origin_arch, ArrayList<Module> modules_arr) {
         ArrayList<Architecture> return_Architectures = new ArrayList<>();
         ArrayList<Integer> modd_arr_sellected = new ArrayList<>();
         //Все устанавливаем в 1
-        for (int s_mod = 0; s_mod < modules_arr_not_selected.size(); s_mod++) {
+        for (int s_mod = 0; s_mod < modules_arr.size(); s_mod++) {
             modd_arr_sellected.add(s_mod, 0);
         }
-        combine(return_Architectures, origin_arch, modules_arr_not_selected, modd_arr_sellected, modd_arr_sellected.size());
+        combine(return_Architectures, origin_arch, modules_arr, modd_arr_sellected, modd_arr_sellected.size());
         return return_Architectures;
     }
 
@@ -31,6 +31,13 @@ public class gen_arch_done {
             } else {
                 modules_arr.get(s_pos).selected_patern = modules_arr.get(s_pos).avilable_paterns.get(modd_arr_sell.get(s_pos));
                 //save
+                int s_add = 0;
+                for (int s_lay = 0; s_lay < origin_arch.layers.size(); s_lay++) {
+                    for (int s_mod = 0; s_mod < origin_arch.layers.get(s_lay).modules.size(); s_mod++) {
+                        origin_arch.layers.get(s_lay).modules.get(s_mod).selected_patern = modules_arr.get(s_add).selected_patern;
+                        s_add++;
+                    }
+                }
                 return_Architectures.add(origin_arch);
                 modd_arr_sell.set(s_pos, modd_arr_sell.get(s_pos) + 1);
                 combine(return_Architectures, origin_arch, modules_arr, modd_arr_sell, modd_arr_sell.size());
