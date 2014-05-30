ef84cb40d133a769524d60395cc37b626fcd7b7d
AlxShcherbak <akexium@gmail.com>
2014-05-30 10:33:06 +0300

    правки ...

--- src/Run/Main.java
+++ src/Run/Main.java
@@ -24,7 +24,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("/mode_selection/Selection_mode.fxml"));
-        primaryStage.setTitle("Selection Mode");
+        primaryStage.setTitle("АРХІТЕКТОР ПРОГРАМНИХ СИСТЕМ");
         primaryStage.setScene(new Scene(root, 700, 500));
         primaryStage.show();
     }
