8b1a1b19cd742e0df46356ccd9b910d4443f3f31
godexxx <godexxxx@gmail.com>
2014-10-08 14:59:00 +0300

    (*) исправления для сборки

--- src/editor/interfaces/Configuration.java
+++ src/editor/interfaces/Configuration.java
@@ -4,6 +4,7 @@ import editor.services.FileUtils;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileWriter;
 import java.util.Locale;
 import java.util.Properties;
 import java.util.ResourceBundle;
@@ -20,11 +21,24 @@ public interface Configuration {
         public Integer drawThreads;
 
         public config() {
+            System.out.println("Init Configuration");
             try {
                 File config = new File("config.properties");
+                config.createNewFile();
+
+                //TODO problem
+                FileWriter configwrite= new FileWriter(config);
+                configwrite.write("language = ua_UA\n" +
+                        "drawThreads = 2");
+
+                configwrite.close();
                 if (!config.exists()) {
+
+                    System.out.println("Configuration file copy");
                     FileUtils.copyFileUsingStream(new File(getClass().getClassLoader().getResource("default_config.properties").toURI()), config);
+
                 }
+
                 FileInputStream fis;
                 Properties property = new Properties();
                 fis = new FileInputStream(config);
